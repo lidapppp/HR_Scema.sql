88b9ec7739e71b6bfa28eea31b4c4717b57a10d0
Muhammad Nur Ashiddiqi <43430326+Tnembull01@users.noreply.github.com>
2019-12-07 23:05:37 +0700

    Update HR_Schema.sql

--- Semester 3/Materi BASDAT/Tugas HR dan OE/HR_Schema.sql
+++ Semester 3/Materi BASDAT/Tugas HR dan OE/HR_Schema.sql
@@ -1,110 +1,130 @@
-CREATE TABLE daerah (
-	id_daerah INT (11) UNSIGNED NOT NULL,
-	nama_daerah VARCHAR(25),
-	PRIMARY KEY (id_daerah)
-	);
-
-CREATE TABLE negara (
-	id_negara  CHAR(2) NOT NULL,
-	nama_negara  VARCHAR(40),
-	id_negara  INT (11) UNSIGNED NOT NULL,
-	PRIMARY KEY (id_negara)
-);
-
-
-CREATE TABLE lokasi (
-	id_lokasi INT (11) UNSIGNED NOT NULL AUTO_INCREMENT,
-	alamat_jalan VARCHAR(40),
-	kode_pos VARCHAR(12),
-	kota VARCHAR(30) NOT NULL,
-	status_provinsi VARCHAR(25),
-	id_negara CHAR(2) NOT NULL,
-	PRIMARY KEY (id_lokasi)
-	);
-
-CREATE TABLE departemen (
-	id_departemen INT (11) UNSIGNED NOT NULL,
-	nama_departemen VARCHAR(30) NOT NULL,
-	id_manajer INT (11) UNSIGNED,
-	id_lokasi INT (11) UNSIGNED,
-	PRIMARY KEY (id_departemen)
-	);
-
-CREATE TABLE pekerjaan (
-	id_pekerjaan VARCHAR(10) NOT NULL,
-	judul_pekerjaan VARCHAR(35) NOT NULL,
-	gaji_minimum DECIMAL(8, 0) UNSIGNED,
-	gaji_maksimum DECIMAL(8, 0) UNSIGNED,
-	PRIMARY KEY (id_pekerjaan)
-	);
-
-CREATE TABLE karyawan(
-	id_karyawan INT (11) UNSIGNED NOT NULL,
-	nama_pertama VARCHAR(20),
-	nama_terakhir VARCHAR(25) NOT NULL,
-	email VARCHAR(25) NOT NULL,
-	nomor_ponsel VARCHAR(20),
-	tanggal_rekrut DATE NOT NULL,
-	id_pekerjaan VARCHAR(10) NOT NULL,
-	gaji DECIMAL(8, 2) NOT NULL,
-	persen_komisi DECIMAL(2, 2),
-	id_manajer INT (11) UNSIGNED,
-	id_departemen INT (11) UNSIGNED,
-	PRIMARY KEY (id_karyawan)
-	);
-
-CREATE TABLE riwayat_pekerjaan (
-	id_pekerjaan INT (11) UNSIGNED NOT NULL,
-	tanggal_mulai DATE NOT NULL,
-	tanggal_akhir DATE NOT NULL,
-	id_pekerjaan VARCHAR(10) NOT NULL,
-	id_departemen INT (11) UNSIGNED NOT NULL
-	);
-
-ALTER TABLE riwayat_pekerjaan ADD UNIQUE INDEX (
-	id_karyawan,
-	tanggal_mulai
-	);
-
-
-CREATE VIEW melihat_detail_karyawan
-AS
-SELECT e.id_karyawan,
-	e.id_pekerjaan,
-	e.id_manajer,
-	e.id_departemen,
-	d.id_lokasi,
-	l.id_negara,
-	e.nama_pertama,
-	e.nama_terakhir,
-	e.gaji,
-	e.persen_komisi,
-	d.nama_departemen,
-	j.judul_pekerjaan,
-	l.kota,
-	l.status_provinsi,
-	c.nama_negara,
-	r.nama_wilayah
-FROM karyawan e,
-	departemen d,
-	pekerjaan j,
-	lokasi l,
-	negara c,
-	wilayah r
-	
-WHERE e.id_karyawan = d.id_karyawan
-	AND d.id_lokasi = l.id_lokasi
-	AND l.id_negara = c.id_negara
-	AND c.id_wilayah = r.id_wilayah
-	AND j.id_pekerjaan = e.id_pekerjaan;
-
-ALTER TABLE negara ADD FOREIGN KEY (id_wilayah) REFERENCES wilayah(id_wilayah);    
-ALTER TABLE lokasi ADD FOREIGN KEY (id_negara) REFERENCES negara(id_negara);
-ALTER TABLE departemen ADD FOREIGN KEY (id_lokasi) REFERENCES lokasi(id_lokasi);
-ALTER TABLE karyawan ADD FOREIGN KEY (id_pekerjaan) REFERENCES pekerjaan(id_pekerjaan);
-ALTER TABLE karyawan ADD FOREIGN KEY (id_departemen) REFERENCES departemen(id_departemen);
-ALTER TABLE karyawan ADD FOREIGN KEY (id_manajer) REFERENCES karyawan(id_karyawan);
-ALTER TABLE departemen ADD FOREIGN KEY (id_manajer) REFERENCES karyawan (id_karyawan);
-ALTER TABLE riwayat_pekerjaan ADD FOREIGN KEY (id_karyawan) REFERENCES karyawan(id_karyawan);
-ALTER TABLE riwayat_pekerjaan ADD FOREIGN KEY (id_pekerjaan) REFERENCES pekerjaan(id_pekerjaan);
-ALTER TABLE riwayat_pekerjaan ADD FOREIGN KEY (id_departemen) REFERENCES departemen(id_departemen);
+CREATE TABLE  "DAERAH" 
+   (	"DAERAH_ID" NUMBER CONSTRAINT "DAERAH_ID_NN" NOT NULL ENABLE, 
+	"DAERAH_NAMA" VARCHAR2(25), 
+	 CONSTRAINT "DAE_ID_PK" PRIMARY KEY ("DAERAH_ID")
+  USING INDEX  ENABLE
+   )
+/
+======================================================================
+CREATE TABLE  "NEGARA" 
+   (	"NEGARA_ID" CHAR(2) CONSTRAINT "NEGARA_ID_NN" NOT NULL ENABLE, 
+	"NEGARA_NAMA" VARCHAR2(40), 
+	"DAERAH_ID" NUMBER, 
+	 CONSTRAINT "NEGARA_C_ID_PK" PRIMARY KEY ("NEGARA_ID")
+  USING INDEX  ENABLE
+   )
+/
+ALTER TABLE  "NEGARA" ADD CONSTRAINT "NEG_DAE_FK" FOREIGN KEY ("DAERAH_ID")
+	  REFERENCES  "DAERAH" ("DAERAH_ID") ENABLE
+/
+==========================================================================
+CREATE TABLE  "LOKASI" 
+   (	"LOKASI_ID" NUMBER(4,0), 
+	"JALAN_ALAMAT" VARCHAR2(40), 
+	"POS_KODE" VARCHAR2(12), 
+	"KOTA" VARCHAR2(30) CONSTRAINT "LOK_KOTA_NN" NOT NULL ENABLE, 
+	"STATUS_PROVINSI" VARCHAR2(25), 
+	"NEGARA_ID" CHAR(2), 
+	 CONSTRAINT "LOK_ID_PK" PRIMARY KEY ("LOKASI_ID")
+  USING INDEX  ENABLE
+   )
+/
+ALTER TABLE  "LOKASI" ADD CONSTRAINT "LOK_C_ID_FK" FOREIGN KEY ("NEGARA_ID")
+	  REFERENCES  "NEGARA" ("NEGARA_ID") ENABLE
+/
+CREATE INDEX  "LOK_KOTA_IX" ON  "LOKASI" ("KOTA")
+/
+CREATE INDEX  "LOK_NEGARA_IX" ON  "LOKASI" ("NEGARA_ID")
+/
+CREATE INDEX  "LOC_STATUS_PROVINSI_IX" ON  "LOKASI" ("STATUS_PROVINSI")
+/
+===============================================================================
+CREATE TABLE  "DEPARTEMEN" 
+   (	"DEPARTEMEN_ID" NUMBER(4,0), 
+	"DEPARTEMEN_NAMA" VARCHAR2(30) CONSTRAINT "DEPT_NAMA_NN" NOT NULL ENABLE, 
+	"MANAJER_ID" NUMBER(6,0), 
+	"LOKASI_ID" NUMBER(4,0), 
+	 CONSTRAINT "DEPT_ID_PK" PRIMARY KEY ("DEPARTEMEN_ID")
+  USING INDEX  ENABLE
+   )
+/
+ALTER TABLE  "DEPARTEMEN" ADD CONSTRAINT "DEPT_LOK_FK" FOREIGN KEY ("LOKASI_ID")
+	  REFERENCES  "LOKASI" ("LOKASI_ID") ENABLE
+/
+ALTER TABLE  "DEPARTEMEN" ADD CONSTRAINT "DEPT_MJR_FK" FOREIGN KEY ("MANAJER_ID")
+	  REFERENCES  "KARYAWAN" ("KARYAWAN_ID") ENABLE
+/
+CREATE INDEX  "DEPT_LOKASI_IX" ON  "DEPARTEMEN" ("LOKASI_ID")
+/
+==================================================================================
+CREATE TABLE  "PERKERJAAN_RIWAYAT" 
+   (	"KARYAWAN_ID" NUMBER(6,0) CONSTRAINT "PERIW_KARYAWAN_NN" NOT NULL ENABLE, 
+	"AWAL_TANGGAL" DATE CONSTRAINT "PERIW_AWAL_TANGGAL_NN" NOT NULL ENABLE, 
+	"AKHIR_TANGGAL" DATE CONSTRAINT "PERIW_AKHIR_TANGGGAL_NN" NOT NULL ENABLE, 
+	"PEKERJAAN_ID" VARCHAR2(10) CONSTRAINT "PERIW_PEKERJAAN_NN" NOT NULL ENABLE, 
+	"DEPARTEMEN_ID" NUMBER(4,0), 
+	 CONSTRAINT "PERIW_TANGGAL_JARAKWAKTU" CHECK (end_date > start_date) ENABLE, 
+	 CONSTRAINT "PERIW_KAR_ID_AW_TANGGAL_PK" PRIMARY KEY ("KARYAWAN_ID", "AWAL_TANGGAL")
+  USING INDEX  ENABLE
+   )
+/
+ALTER TABLE  "PEKERJAAN_RIWAYAT" ADD CONSTRAINT "PERIW_DEPT_FK" FOREIGN KEY ("DEPARTEMEN_ID")
+	  REFERENCES  "DEPARTEMEN" ("DEPARTEMEN_ID") ENABLE
+/
+ALTER TABLE  "PEKERJAAN_RIWAYAT" ADD CONSTRAINT "PERIW_KAR_FK" FOREIGN KEY ("KARYAWAN_ID")
+	  REFERENCES  "KARYAWAN" ("KARYAWAN_ID") DISABLE
+/
+ALTER TABLE  "PEKERJAAN_RIWAYAT" ADD CONSTRAINT "PERIW_PEKERJAAN_FK" FOREIGN KEY ("PEKERJAAN_ID")
+	  REFERENCES  "PEKERJAAN" ("PEKERJAAN_ID") ENABLE
+/
+CREATE INDEX  "PERIW_DEPARTEMEN_IX" ON  "PEKERJAAN_RIWAYAT" ("DEPARTEMEN_ID")
+/
+==============================================================================================
+CREATE TABLE  "PEKERJAAN" 
+   (	"PEKERJAAN_ID" VARCHAR2(10), 
+	"PEKERJAAN_JUDUL" VARCHAR2(35) CONSTRAINT "PEKERJAAN_JUDUL_NN" NOT NULL ENABLE, 
+	"MINIMUM_GAJI" NUMBER(6,0), 
+	"MAKSIMUM_GAJI" NUMBER(6,0), 
+	 CONSTRAINT "PEKERJAAN_ID_PK" PRIMARY KEY ("PEKERJAAN_ID")
+  USING INDEX  ENABLE
+   )
+/
+==============================================================================================
+CREATE TABLE  "KARYAWAN" 
+   (	"KARYAWAN_ID" NUMBER(6,0), 
+	"AWAL_NAMA" VARCHAR2(20), 
+	"AKHIR_NAME" VARCHAR2(25) CONSTRAINT "KAR_AWAL_NAMA_NN" NOT NULL ENABLE, 
+	"EMAIL" VARCHAR2(25) CONSTRAINT "KAR_EMAIL_NN" NOT NULL ENABLE, 
+	"PONSEL_NOMOR" VARCHAR2(20), 
+	"REKRUT_TANGGAL" DATE CONSTRAINT "KAR_REKRUT_TANGGAL_NN" NOT NULL ENABLE, 
+	"PEKERJAAN_ID" VARCHAR2(10) CONSTRAINT "KAR_PEKERJAAN_NN" NOT NULL ENABLE, 
+	"GAJI" NUMBER(8,2), 
+	"KOMISI_PERSEN" NUMBER(2,2), 
+	"MANAJER_ID" NUMBER(6,0), 
+	"DEPARTEMENT_ID" NUMBER(4,0), 
+	"BONUS" VARCHAR2(5), 
+	 CONSTRAINT "KAR_GAJI_MINIMUM" CHECK (gaji > 0) ENABLE, 
+	 CONSTRAINT "KAR_ID_PK" PRIMARY KEY ("KARYAWAN_ID")
+  USING INDEX  ENABLE, 
+	 CONSTRAINT "KAR_EMAIL_UK" UNIQUE ("EMAIL")
+  USING INDEX  ENABLE
+   )
+/
+ALTER TABLE  "KARYAWAN" ADD CONSTRAINT "KAR_DEPT_FK" FOREIGN KEY ("DEPARTEMEN_ID")
+	  REFERENCES  "DEPARTEMEN" ("DEPARTEMEN_ID") ENABLE
+/
+ALTER TABLE  "KARYAWAN" ADD CONSTRAINT "KAR_PEKERJAAN_FK" FOREIGN KEY ("PEKERJAAN_ID")
+	  REFERENCES  "JPEKERJAAN" ("PEKERJAAN_ID") ENABLE
+/
+ALTER TABLE  "KARYAWAN" ADD CONSTRAINT "KAR_MANAJER_FK" FOREIGN KEY ("MANAJER_ID")
+	  REFERENCES  "KARYAWAN" ("KARYAWAN_ID") ENABLE
+/
+CREATE INDEX  "KAR_DEPARTEMEN_IX" ON  "KARYAWAN" ("DEPARTEMEN_ID")
+/
+CREATE INDEX  "KAR_PEKERJAAN_IX" ON  "KARYAWAN" ("PEKERJAAN_ID")
+/
+CREATE INDEX  "KAR_MANAJER_IX" ON  "KARYAWAN" ("MANAJER_ID")
+/
+CREATE INDEX  "KAR_NAMA_IX" ON  "KARYAWAN" ("AKHIR_NAMA", "AWAL_NAMA")
+/
+===========================================================================================
