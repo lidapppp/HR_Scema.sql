fca2a8a6db324ce8209510b9aa42cd56be1e38da
Muhammad Nur Ashiddiqi <43430326+Tnembull01@users.noreply.github.com>
2019-12-08 18:38:04 +0700

    Update HR.sql

--- Semester 3/Materi BASDAT/HR Schema/HR.sql
+++ Semester 3/Materi BASDAT/HR Schema/HR.sql
@@ -2,14 +2,7 @@ CREATE TABLE adalah perintah yang digunakan untuk membuat sebuah table di databa
 ALTER TABLE adalah perintah di database MySQL untuk merubah struktur table baik itu merubah nama table, merubah nama kolom, merubah tipe data, dan lain-lain.
 CREATE INDEX  adalah perintah di mysql unutuk membuat sebuah objek dalam sistem database yang dapat mempercepat proses pencarian (query) data. 
 CONSTRAINT adalah perintah untuk membuat batasan atau aturan yang ada pada table.
-
-
-
-
-
-
-
-
+==============================================================================================================================================================
 CREATE TABLE  "DAERAH" 
    (	"DAERAH_ID" NUMBER CONSTRAINT "DAERAH_ID_NN" NOT NULL ENABLE, 
 	"DAERAH_NAMA" VARCHAR2(25), 
@@ -22,7 +15,7 @@ CREATE TABLE  "NEGARA"
    (	"NEGARA_ID" CHAR(2) CONSTRAINT "NEGARA_ID_NN" NOT NULL ENABLE, 
 	"NEGARA_NAMA" VARCHAR2(40), 
 	"DAERAH_ID" NUMBER, 
-	 CONSTRAINT "NEGARA_C_ID_PK" PRIMARY KEY ("NEGARA_ID")
+	 CONSTRAINT "NEGARA_N_ID_PK" PRIMARY KEY ("NEGARA_ID")
   USING INDEX  ENABLE
    )
 /
@@ -48,7 +41,7 @@ CREATE INDEX  "LOK_KOTA_IX" ON  "LOKASI" ("KOTA")
 /
 CREATE INDEX  "LOK_NEGARA_IX" ON  "LOKASI" ("NEGARA_ID")
 /
-CREATE INDEX  "LOC_STATUS_PROVINSI_IX" ON  "LOKASI" ("STATUS_PROVINSI")
+CREATE INDEX  "LOK_STATUS_PROVINSI_IX" ON  "LOKASI" ("STATUS_PROVINSI")
 /
 ===============================================================================
 CREATE TABLE  "DEPARTEMEN" 
@@ -105,7 +98,7 @@ CREATE TABLE  "PEKERJAAN"
 CREATE TABLE  "KARYAWAN" 
    (	"KARYAWAN_ID" NUMBER(6,0), 
 	"AWAL_NAMA" VARCHAR2(20), 
-	"AKHIR_NAME" VARCHAR2(25) CONSTRAINT "KAR_AWAL_NAMA_NN" NOT NULL ENABLE, 
+	"AKHIR_NAMA" VARCHAR2(25) CONSTRAINT "KAR_AWAL_NAMA_NN" NOT NULL ENABLE, 
 	"EMAIL" VARCHAR2(25) CONSTRAINT "KAR_EMAIL_NN" NOT NULL ENABLE, 
 	"PONSEL_NOMOR" VARCHAR2(20), 
 	"REKRUT_TANGGAL" DATE CONSTRAINT "KAR_REKRUT_TANGGAL_NN" NOT NULL ENABLE, 
