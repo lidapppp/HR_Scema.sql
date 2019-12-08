6587e14b99b458d188abb861351eb04ba80d5a4b
Muhammad Nur Ashiddiqi <43430326+Tnembull01@users.noreply.github.com>
2019-12-08 20:26:38 +0700

    Update HR_Schema.sql

--- Semester 3/Materi BASDAT/Tugas HR dan OE/HR_Schema.sql
+++ Semester 3/Materi BASDAT/Tugas HR dan OE/HR_Schema.sql
@@ -13,52 +13,6 @@ CREATE TABLE pegawai (
 	BONUS VARCHAR2(5),
 	CONSTRAINT peg_id_pegawai_pk PRIMARY KEY (id_pegawai)
 )
-
-INSERT ALL 
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (100, 'Steven', 'King', 'SKING', '515.123.4567', '17-Jun-1987','AD_PRES',24000, '', '', 90, '')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (101, 'Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', '21-Sep-1989','AD_VP',17000,'',100,90,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (102, 'Lex', 'De Haan', 'LDEHAAN', '515.123.4569', '13-Jan-1993','AD_VP',17000,'',100,90,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (200, 'Jennifer', 'Whalen', 'JWHALEN', '515.123.4444', '17-Sep-1987','AD_ASST',4400,'',101,10,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (205, 'Shelley', 'Higgins', 'SHIGGINS', '515.123.8080', '07-Jun-1994','AC_MGR',12000,'',101,110,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (206, 'William', 'Gietz', 'WGIETZ', '515.123.8181', '07-Jun-1994','AC_ACCOUNT',8300,'',205,110, '')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (149, 'Eleni', 'Zlotkey', 'EZLOTKEY', '011.44.1344.429018', '29-Jan-2000','SA_MAN',10500,0.2,100,80,1500)
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (174, 'Ellen', 'Abel', 'EABEL', '011.44.1644.429267', '11-May-1996','SA_REP'	11000,0.3,149,80,1700)
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (176, 'Jonathon', 'Taylor', 'JTAYLOR', '011.44.1644.429265', '24-Mar-1998','SA_REP',8600,0.2,149, 80,1250)
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (178, 'Kimberely', 'Grant', 'KGRANT', '011.44.1644.429263', '24-May-1999','SA_REP',7000,0.15,149,'','')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (124, 'Kevin', 'Mourgos', 'KMOURGOS', '650.123.5234', '16-Nov-1999','ST_MAN',5800,'',100,50,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (141, 'Trenna', 'Rajs', 'TRAJS', '650.121.8009', '17-Oct-1995','ST_CLERK',3500,'',124,50,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (142, 'Curtis', 'Davies', 'CDAVIES', '650.121.2994', '29-Jan-1997','ST_CLERK',3100,'',124,50,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (143, 'Randall', 'Matos', 'RMATOS', '650.121.2874', '15-Mar-1998','ST_CLERK',2600,'',124,50,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (144, 'Peter', 'Vargas', 'PVARGAS', '650.121.2004', '09-Jul-1998','ST_CLERK', 2500,'',124,50,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (103, 'Alexander', 'Hunold', 'AHUNOLD', '590.423.4567', '03-Jan-1990','IT_PROG',9000,'',102,60,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (104, 'Bruce', 'Ernst', 'BERNST', '590.423.4568', '21-May-1991','IT_PROG',6000,'',103,60,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (107, 'Diana', 'Lorentz', 'DLORENTZ', '590.423.5567', '07-Feb-1999','IT_PROG',4200,'',103,60,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (201, 'Michael', 'Hartstein', 'MHARTSTE', '515.123.5555', '17-Feb-1996','MK_MAN',13000,'',100,20,'')
-	INTO pegawai(id_pegawai, nama_depan, nama_belakang, email, no_telepon, tanggal_diterima, id_pekerjaan, gaji, komisi, id_manager, id_departemen, bonus) 
-	VALUES (202, 'Pat', 'Fay'	'PFAY', '603.123.6666', '17-Aug-1997', 'MK_REP',6000,'',201,20,'')
-
-SELECT * FROM DUAL;
-
-
 ------------------------------------------------
 
 CREATE TABLE departemen (
@@ -69,24 +23,6 @@ CREATE TABLE departemen (
 	CONSTRAINT id_depat_pk PRIMARY KEY (id_departemen)	
 );
 
-INSERT ALL
-	INTO departemen (id_departemen, nama_departemen, id_manager, id_lokasi)
-	VALUES (10, Administration, 200, 1700)
-	INTO departemen (id_departemen, nama_departemen, id_manager, id_lokasi)
-	VALUES (20, Marketing, 201, 1800)
-	INTO departemen (id_departemen, nama_departemen, id_manager, id_lokasi)
-	VALUES (50, Shipping, 124, 1500)
-	INTO departemen (id_departemen, nama_departemen, id_manager, id_lokasi)
-	VALUES (60, IT, 103, 1400)
-	INTO departemen (id_departemen, nama_departemen, id_manager, id_lokasi)
-	VALUES (80, Sales, 149, 2500)
-	INTO departemen (id_departemen, nama_departemen, id_manager, id_lokasi)
-	VALUES (90, Executive, 100, 1700)
-	INTO departemen (id_departemen, nama_departemen, id_manager, id_lokasi)
-	VALUES (110, Accounting, 205, 1700)
-	INTO departemen (id_departemen, nama_departemen, id_manager, id_lokasi)
-	VALUES (190, Contracting, '', 1700)
-SELECT * FROM DUAL;
 
 -----------------------------------------------------------------------------
 
@@ -98,32 +34,6 @@ CREATE TABLE pekerjaan (
 	CONSTRAINT id_pekerjaan_pk PRIMARY KEY (id_pekerjaan)
 );
 
-INSERT ALL
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('AD_PRES', 'President', 20000, 40000)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('AD_VP', 'Administration Vice President', 15000, 30000)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('AD_ASST', 'Administration Assistant', 3000, 6000)('AC_MGR', 'Accounting Manager', 8200, 16000)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('AC_MGR', 'Accounting Manager', 8200, 16000)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('AC_ACCOUNT', 'Public Accountant', 4200, 9000)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('SA_MAN', 'Sales Manager', 10000, 20000)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('SA_REP', 'Sales Representative', 6000, 12000)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('ST_MAN', 'Stock Manager', 5500, 8500)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('ST_CLERK', 'Stock Clerk', 2000, 5000)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('IT_PROG', 'Programmer', 4000, 10000)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('MK_MAN', 'Marketing Manager', 9000, 15000)
-	INTO pekerjaan (id_pekerjaan, jabatan, gaji_min, gaji_max)
-	VALUES ('MK_REP', 'Marketing Representative', 4000, 9000)
-SELECT * FROM DUAL;
 
 -------------------------------------------------------------------------
 
@@ -136,28 +46,6 @@ CREATE TABLE riwayat_pekerjaan (
 	CONSTRAINT riw_id_peg_tgl_mulai_pk PRIMARY KEY (id_pegawai, tanggal_mulai)
 )
 
-INSERT ALL
-	INTO riwayat_pekerjaan (id_pegawai, tanggal_mulai, tanggal_selesai, id_pekerjaan, id_departemen)
-	VALUES (200, '17-Sep-1987', '17-Jun-1993', 'AD_ASST', 90)
-	INTO riwayat_pekerjaan (id_pegawai, tanggal_mulai, tanggal_selesai, id_pekerjaan, id_departemen)
-	VALUES (101, '28-Oct-1993', '15-Mar-1997', 'AC_MGR', 110)
-	INTO riwayat_pekerjaan (id_pegawai, tanggal_mulai, tanggal_selesai, id_pekerjaan, id_departemen)
-	VALUES (200, '01-Jul-1994', '31-Dec-1998', 'AC_ACCOUNT', 90)
-	INTO riwayat_pekerjaan (id_pegawai, tanggal_mulai, tanggal_selesai, id_pekerjaan, id_departemen)
-	VALUES (101, '21-Sep-1989', '27-Oct-1993', 'AC_ACCOUNT', 110)
-	INTO riwayat_pekerjaan (id_pegawai, tanggal_mulai, tanggal_selesai, id_pekerjaan, id_departemen)
-	VALUES (176, '01-Jan-1999', '31-Dec-1999', 'SA_MAN', 80)
-	INTO riwayat_pekerjaan (id_pegawai, tanggal_mulai, tanggal_selesai, id_pekerjaan, id_departemen)
-	VALUES (176, '24-Mar-1998', '31-Dec-1998', 'SA_REP', 80)
-	INTO riwayat_pekerjaan (id_pegawai, tanggal_mulai, tanggal_selesai, id_pekerjaan, id_departemen)
-	VALUES (122, '01-Jan-1999', '31-Dec-1999', 'ST_CLERK', 50)
-	INTO riwayat_pekerjaan (id_pegawai, tanggal_mulai, tanggal_selesai, id_pekerjaan, id_departemen)
-	VALUES (114, '24-Mar-1998', '31-Dec-1999', 'ST_CLERK', 50)
-	INTO riwayat_pekerjaan (id_pegawai, tanggal_mulai, tanggal_selesai, id_pekerjaan, id_departemen)
-	VALUES (102, '13-Jan-1993', '24-Jul-1998', 'IT_PROG', 60)
-	INTO riwayat_pekerjaan (id_pegawai, tanggal_mulai, tanggal_selesai, id_pekerjaan, id_departemen)
-	VALUES (201, '17-Feb-1996', '19-Dec-1999', 'MK_REP', 20)
-SELECT * FROM DUAL;
 
 -----------------------------------------------------------------------------------------------------------------
 
@@ -171,18 +59,6 @@ CREATE TABLE lokasi (
 	CONSTRAINT id_lokasi_pk PRIMARY KEY (id_lokasi)
 );
 
-INSERT ALL
-	INTO lokasi (id_lokasi, nama_jalan, kode_pos, kota, provinsi, id_negara)
-	VALUES 1800, '460 Bloor St. W.', 'ON M5S 1X8', 'Toronto', 'Ontario', 'CA'
-	INTO lokasi (id_lokasi, nama_jalan, kode_pos, kota, provinsi, id_negara)
-	VALUES 2500, 'Magdalen Centre, The Oxford Science Park', 'OX9 9ZB', 'Oxford', 'Oxford', 'UK'
-	INTO lokasi (id_lokasi, nama_jalan, kode_pos, kota, provinsi, id_negara)
-	VALUES 1400, '2014 Jabberwocky Rd', '26192', 'Southlake', 'Texas', 'US'
-	INTO lokasi (id_lokasi, nama_jalan, kode_pos, kota, provinsi, id_negara)
-	VALUES 1500, '2011 Interiors Blvd', '99236', 'South San Francisco', 'California', 'US'
-	INTO lokasi (id_lokasi, nama_jalan, kode_pos, kota, provinsi, id_negara)
-	VALUES 1700, '2004 Charade Rd', '98199', 'Seattle', 'Washington', 'US'
-SELECT * FROM DUAL;
 
 -------------------------------------------------------------------------------------------------
 
@@ -193,16 +69,7 @@ CREATE TABLE negara (
 	CONSTRAINT neg_id_negara_pk PRIMARY KEY (id_negara)
 );
 
-INSERT ALL
-	INTO negara (id_negara, nama_negara, id_daerah)
-	VALUES ('CA', 'Canada', 2)
-	INTO negara (id_negara, nama_negara, id_daerah)
-	VALUES ('DE', 'Germany', 1)
-	INTO negara (id_negara, nama_negara, id_daerah)
-	VALUES ('UK', 'United Kingdom', 1s)
-	INTO negara (id_negara, nama_negara, id_daerah)
-	VALUES ('US', 'United States of America', 2)
-SELECT * FROM DUAL;
+
 
 ----------------------------------------------------------------------------------------------
 
@@ -211,18 +78,6 @@ CREATE TABLE daerah (
 	nama_daerah VARCHAR2(25),
 	CONSTRAINT id_daerah_pk PRIMARY KEY (id_daerah)
 );
-
-INSERT ALL
-	INTO daerah (id_daerah, nama_daerah)
-	VALUES (1, 'Europe')
-	INTO daerah (id_daerah, nama_daerah)
-	VALUES (2, 'Americas')
-	INTO daerah (id_daerah, nama_daerah)
-	VALUES (3, 'Asia')
-	INTO daerah (id_daerah, nama_daerah)
-	VALUES (4, 'Middle East and Africa')
-SELECT * FROM DUAL;
-
 
 ------------------------------------------------------------------------------------
 
